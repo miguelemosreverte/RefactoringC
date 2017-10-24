959777bb98175d701fdf16367dabf1eb574e0d29
miguelemosreverte <miguellemos95@gmail.com>
2017-10-24 18:27:16 -0300

    Added all files

---
+++ YAML-for-setting-decoupling2/Report.py
@@ -0,0 +1,86 @@
+import os
+import time
+import sys
+from os.path import join
+import yaml
+from datetime import datetime
+import shutil
+import copy
+import pprint
+
+class ProfileReporter:
+
+	def __init__(self):
+		# Read YAML file
+		with open("commands.yaml", 'r') as stream:
+			data_loaded = yaml.load(stream)
+			self.output_dir = data_loaded["output_dir"]
+			self.save = data_loaded["save"]
+			self.filenames = data_loaded["filenames"]
+			self.commands = data_loaded["commands"]
+			self.cleanup = data_loaded["cleanup"]
+
+		self.create_output_folder()
+		self.apply_commands_transformations()
+		#pprint.pprint(self.commands)
+
+	def create_output_folder(self):
+		if not os.path.exists(self.output_dir):
+			os.makedirs(self.output_dir)
+		self.output_dir = self.output_dir + datetime.now().strftime("%H:%M:%S.%f") + "/"
+		if not os.path.exists(self.output_dir):
+			os.makedirs(self.output_dir)
+
+	def apply_commands_transformations(self):
+		for command_category in self.commands:
+			for command_name in self.commands[command_category]:
+				template_command = self.commands[command_category][command_name]
+				#we create a Many To Many relationship between commands and filenames now
+				self.commands[command_category][command_name] = {}
+				for filename in self.filenames:
+					command_with_specified_filename = \
+						template_command.replace("source_filename", filename)\
+						.replace("report_filename", self.output_dir + os.path.splitext(filename)[0]+'.txt')
+				 	self.commands[command_category][command_name][filename] = command_with_specified_filename
+
+	def run_main(self):
+		for filename in self.filenames:
+			for command in self.commands["main"]:
+				self.run_command(self.commands["main"][command][filename], filename)
+		self.run_cleanup()
+
+	def echo_command_title(self, command):
+		return 'echo "\n'+'-'*50+'\n'+command+'"'
+	def run_command(self,command, filename):
+		output_file =  os.path.splitext(filename)[0]+'.txt'
+		command_title = self.echo_command_title(command)+";"
+		command = self.save\
+			.replace("command", command_title + command)\
+			.replace("output_file", self.output_dir + output_file)
+		os.system(command)
+
+	def run_report(self, output_report_file):
+		report_commands = ""
+		for item in os.listdir(self.output_dir):
+			commands = "( insert_commands_here ) >> " +  self.output_dir + output_report_file
+			report_commands = [
+							self.commands["report"][command_names][filename]
+							for command_names in self.commands["report"]
+							for filename in self.commands["report"][command_names]
+							]
+			report_commands = zip([self.echo_command_title(command) for command in report_commands],report_commands)
+			report_commands = ";".join([";".join(list(value)) for value in report_commands])+";"
+		os.system(commands.replace("insert_commands_here", report_commands))
+
+	def run_cleanup(self):
+		extensions = self.cleanup["extensions"]
+		dir = "./"
+		for item in os.listdir(dir):
+		    for extension in extensions:
+			    if item.endswith(extension):
+				os.remove(join(dir, item))
+
+if __name__ == "__main__":
+	MyProfileReporter = ProfileReporter()
+	MyProfileReporter.run_main()
+	MyProfileReporter.run_report("sinopsis.txt")
---
+++ YAML-for-setting-decoupling2/original.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <list>
+#include "timespan.h"
+using namespace std;
+
+struct linkedTimestamp {
+    float hora, min, seg, bytes, nro_destino, num;
+};
+
+class MyLinkedTimestamp {
+  public:
+
+
+    list <linkedTimestamp> lista;
+
+
+
+
+
+    void toString(){
+      for( auto& item : this->lista){
+        cout  << setw(7) << item.hora
+              << setw(7) << item.min
+              << setw(7) << item.seg
+              << setw(7) << item.bytes
+              << setw(7) << item.nro_destino
+              << endl;
+      }
+    };
+
+
+    void cargarArchivo(string nombre_de_archivo  = "../Archivos/red1.txt"){
+
+        ifstream entrada;
+        entrada.open(nombre_de_archivo);
+
+        if (entrada.is_open()) {
+
+            while (!entrada.eof()) {
+                linkedTimestamp aux;// = new linkedTimestamp;//chequear si es null
+                entrada >> aux.hora;
+                entrada >> aux.min;
+                entrada >> aux.seg;
+                entrada >> aux.bytes;
+                entrada >> aux.nro_destino;
+                this->lista.push_back(aux);
+            }
+
+            entrada.close();
+        }
+
+        //this->toString();
+    };
+
+
+
+
+    //Consigna 4
+    void contarCantidadDePaquetesEnviados(){
+        int cantidad_de_paquetes_enviados = 0;
+        float acummulated_bytes = 0.0;
+        float primer_nro_destino = this->lista.front().nro_destino;
+        cout << "Entrando en contarCantidadDePaquetesEnviados" << endl;
+
+        /*while (this->lista != NULL) {
+            if (this->lista->nro_destino == primer_nro_destino) {
+                cantidad_de_paquetes_enviados++;
+                acummulated_bytes += this->lista->bytes;
+
+            }
+
+            this->lista = this->lista->next;
+
+        }
+
+        cout << "la cantidad de paquetes es: " << cantidad_de_paquetes_enviados << " son = " << acummulated_bytes / 1000 << " kilobytes " << endl;
+        */
+    };
+
+    //Consigna 5
+    void calcularKBpsPromedio(){};
+}myLinkedTimestamps;
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+    linkedTimestamp * aux = NULL;
+
+    int begginingTimestamp = getMilliCount();
+
+    myLinkedTimestamps.cargarArchivo();
+    myLinkedTimestamps.toString();
+    myLinkedTimestamps.contarCantidadDePaquetesEnviados();
+
+    /*if (cargarArchivo() == 1) {
+        cerr << "hubo algun problema interno." << endl;
+    }*/
+
+    cout << "Took " + to_string(getMilliSpan(begginingTimestamp)) +  " milliseconds";
+
+    return 0;
+}
